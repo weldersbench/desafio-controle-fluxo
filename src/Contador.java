733bdb420cfdc97cfe39534faada73e1ee0aa759
Leandro Nunes Alves <leandro.nunes@redeflex.com.br>
2025-05-21 18:49:13 -0300

    Criação das classes

---
+++ src/Contador.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero: ");
+        int primeiroNum = scanner.nextInt();
+
+        System.out.println("Digite o segundo numero: ");
+        int segundoNum = scanner.nextInt();
+
+    }
+}
