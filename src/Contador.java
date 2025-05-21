be2de0a22ef7e1f8273c55d1a8bb096fd370a4cc
Leandro Nunes Alves <leandro.nunes@redeflex.com.br>
2025-05-21 19:54:52 -0300

    Criação do fluxo e logica do sistema

--- src/Contador.java
+++ src/Contador.java
@@ -6,11 +6,30 @@ public class Contador {
 
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.println("Digite o primeiro numero: ");
         int primeiroNum = scanner.nextInt();
 
         System.out.println("Digite o segundo numero: ");
         int segundoNum = scanner.nextInt();
 
+        try{
+            contar(primeiroNum, segundoNum);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    static void contar(int primeiroNum, int segundoNum) throws ParametrosInvalidosException{
+
+        if (segundoNum > primeiroNum){
+            int contagem = segundoNum - primeiroNum;
+
+            for (int i = 1; i <= contagem; i++)
+                System.out.println("Imprimindo os numeros: " + i);
+        }else {
+            throw new ParametrosInvalidosException();
+        }
     }
 }
